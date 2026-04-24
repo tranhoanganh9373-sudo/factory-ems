cd0ee85db5b958f3447291314ed8dc484ba766d7
impl <impl@local>
2026-04-24 20:51:43 +0800

    feat(app): Spring Boot bootstrap + profiles

---
+++ ems-app/src/main/java/com/ems/app/FactoryEmsApplication.java
@@ -0,0 +1,21 @@
+package com.ems.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(scanBasePackages = "com.ems")
+@EntityScan(basePackages = "com.ems")
+@EnableJpaRepositories(basePackages = "com.ems")
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
+public class FactoryEmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryEmsApplication.class, args);
+    }
+}
